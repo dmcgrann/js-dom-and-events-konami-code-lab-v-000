5147f1414fc0628f56452d5a2a19ad660526cc09
David McGrann <dpmcgrann@gmail.com>
2019-01-28 23:11:17 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,14 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(code) {
+  if (code === parseInt(e.detail || e.which)); {
+    index++
+      if (index === 10) {
+        alert("Hurray!");
+        index = 0;
+      }
+  } else {
+    index = 0;
+  }
 
 }
