160523736c1cc3ef3913a26a910ae50d81f62530
David McGrann <dpmcgrann@gmail.com>
2019-01-28 23:16:02 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  document.body.addEventListener(keydown,function(event)
-    const key = 
+  document.body.addEventListener(keydown,function(e)
+    const key = parseInt(e.detail || e.which)
   
   )
   if (code === parseInt(e.detail || e.which)); {
