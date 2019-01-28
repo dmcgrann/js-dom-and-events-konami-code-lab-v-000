49b087e3cfcddaf23f793e573048bc3c9d5d14ca
David McGrann <dpmcgrann@gmail.com>
2019-01-28 23:18:42 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,16 +5,15 @@ function init() {
   let index = 0;
   document.body.addEventListener("keydown",function(e) {
     const key = parseInt(e.detail || e.which);
-  
-  )
-  if (key === code[index]) {
+    
+    if (key === code[index]) {
     index++
       if (index === code.length) {
         alert("Hurray!");
         index = 0;
-      }
-  } else {
+        }
+      } else {
     index = 0;
   }
-
+  );
 }
